380097f26817a56f81dc51142bf45dbafa88105f
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2022-09-09 23:08:38 +0300

    course list

--- AttendlyApp/ViewController.swift
+++ AttendlyApp/ViewController.swift
@@ -7,15 +7,16 @@
 
 import UIKit
 import FirebaseFirestore
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        get()}
-        
+      get()
+    }
       /*  func loadStats(){
-          
             let db = Firestore.firestore()
             db.collection("Unistudent").addDocument(data: ["advisorID" : "441111111", "major" : "SWE","sFirstN" : "Amani","sLastN" : "Aldahmash","studentID" : "441204066"])
             {error in
@@ -26,22 +27,32 @@ class ViewController: UIViewController {
                 print("YESSSSSSSSSSS")
                 }
             }}}*/
+    func get(){
+        let db = Firestore.firestore()
+        db.collection("Unistudent").whereField("Emailstudent", isEqualTo: "mo@gmail.com").getDocuments{
+            (snapshot, error) in
+            if let error = error {
+             print("FAAAAAIIIIILLLLLL ")
+            }
+            else{
+            print("SUCCESSSSS")
+                let actual = snapshot!.documents.first!.get("courses") as! [String]
+                print(actual)
+                for i in 0..<actual.count {
+                
+                    let label = UILabel(frame: .init(x: self.view.frame.midX - 100, y: 200 + ( Double(i) * 70 ), width: 100, height: 50))
+                    label.backgroundColor = .red
+                    label.text = actual[i]
+                    self.view.addSubview(label)
+                }
+                //Vstack
+               // coursesT.text = actual
+           //     print((actual).count)
+            }
+        }
+      //  coursesT.text = actual
+}
 }
 
     
-        func get(){
-            let db = Firestore.firestore()
-            db.collection("Unistudent").document("jhkPjKIZ4mj9ZisvgjFb").getDocument{
-                (document, error) in
-                if let error = error {
-                 print("FAAAAAIIIIILLLLLL ")
-                }
-                else{
-                print("YESSSSSSSSSSS")
-                    let actual = document!.get("courses")
-                    print(actual)
-                    print(Array(arrayLiteral: actual).count)
-                }
-            }
-            
-        }
+
