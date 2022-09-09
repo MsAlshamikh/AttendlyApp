b0138ba6f4a79b55a27d5194c34c598c825c9b48
Raniaalageel <rania.kh.ala@gmail.com>
2022-09-09 18:42:53 +0300

    Rania's edit

--- AttendlyApp/ViewController.swift
+++ AttendlyApp/ViewController.swift
@@ -12,8 +12,10 @@ import CoreNFC
 //import Firebase
 //import FirebaseDatabse
 // just a comment 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
 
+    @IBOutlet weak var NFCText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -47,11 +49,33 @@ class ViewController: UIViewController {
        */
     
     
-    var nfcsession: NFCNDEFReaderSession?
-    var word="none"
+    var nfcSession: NFCNDEFReaderSession?
+    var type="None"
     @IBAction func ScanBtn (_sender:Any){//check in botton
-        nfcsession
+        nfcSession = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.begin()
     }
     
+    override func  didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
   
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("The session was invalidated: \(error.localizedDescription)")
+    }
+    
+    
+    
+    
+    /*func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        var result = ""
+        for payload in messages[0].records{
+            result += String.init(data: payload.payload.advanced(by: 3), encoding: .utf8) ?? "Formate not supported"
+        }
+        DispatchQueue.main.async {
+            self.NFCText.text = result
+        }
+    }*/
+    
+    
 }
