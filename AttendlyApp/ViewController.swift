d146e15ddb59ccda8cae8be6f30f588a1c633985
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2022-09-09 15:57:02 +0300

    tab bar

--- AttendlyApp/ViewController.swift
+++ AttendlyApp/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 import FirebaseFirestore
+
+//import Firebase
+//import FirebaseDatabse
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        loadStats()}
+    
+    func loadStats(){
+      
+        let db = Firestore.firestore()
+        db.collection("Unistudent").addDocument(data: ["name": "Amani", "major" :"SWE"])
+        {(error) in
+            if error != nil{
+                
+            }
+            else{
+                
+            }
+        }}}
 
 
-}
+/*
+    func post(){
+        let db = Firestore.firestore()
+        db.collection("Unistudent").document("jhkPjKIZ4mj9ZisvgjFb")
+        //.getDocument()
+        
+    }*/
 
