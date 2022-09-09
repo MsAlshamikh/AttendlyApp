3aa325373223f57221ba476660c358a1c5067ef4
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2022-09-09 19:51:22 +0300

    read and write from fb

--- AttendlyApp/ViewController.swift
+++ AttendlyApp/ViewController.swift
@@ -12,8 +12,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+        get()}
+        
+      /*  func loadStats(){
+          
+            let db = Firestore.firestore()
+            db.collection("Unistudent").addDocument(data: ["advisorID" : "441111111", "major" : "SWE","sFirstN" : "Amani","sLastN" : "Aldahmash","studentID" : "441204066"])
+            {error in
+                if let error = error {
+                 print("FAAAAAIIIIILLLLLL ")
+                }
+                else{
+                print("YESSSSSSSSSSS")
+                }
+            }}}*/
 }
 
+    
+        func get(){
+            let db = Firestore.firestore()
+            db.collection("Unistudent").document("jhkPjKIZ4mj9ZisvgjFb").getDocument{
+                (document, error) in
+                if let error = error {
+                 print("FAAAAAIIIIILLLLLL ")
+                }
+                else{
+                print("YESSSSSSSSSSS")
+                    let actual = document!.get("courses")
+                    print(actual)
+                    print(Array(arrayLiteral: actual).count)
+                }
+            }
+            
+        }
