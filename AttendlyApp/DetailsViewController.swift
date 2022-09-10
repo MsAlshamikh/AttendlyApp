d3daafea5bca904c2596ed97b832b1dad10fec08
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2022-09-10 19:08:25 +0300

    view details-done

---
+++ AttendlyApp/DetailsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailsViewController.swift
+//  AttendlyApp
+//
+//  Created by Amani Aldahmash on 10/09/2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var sectionLabel: UILabel!
+    @IBOutlet weak var lecturerLabel: UILabel!
+    
+    var section: String = ""
+    var titleB: String = ""
+    var name: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        courseLabel.text = titleB
+   
+        
+        sectionLabel.text = section
+
+        
+        lecturerLabel.text = name
+  
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AttendlyApp/ViewController.swift
+++ AttendlyApp/ViewController.swift
@@ -10,41 +10,45 @@ import FirebaseFirestore
 
 class ViewController: UIViewController {
     
+    var section: String = ""
+    var titleB: String = ""
+    var name: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-      get()
+        get()
     }
-      /*  func loadStats(){
-            let db = Firestore.firestore()
-            db.collection("Unistudent").addDocument(data: ["advisorID" : "441111111", "major" : "SWE","sFirstN" : "Amani","sLastN" : "Aldahmash","studentID" : "441204066"])
-            {error in
-                if let error = error {
-                 print("FAAAAAIIIIILLLLLL ")
-                }
-                else{
-                print("YESSSSSSSSSSS")
-                }
-            }}}*/
+    /*  func loadStats(){
+     let db = Firestore.firestore()
+     db.collection("Unistudent").addDocument(data: ["advisorID" : "441111111", "major" : "SWE","sFirstN" : "Amani","sLastN" : "Aldahmash","studentID" : "441204066"])
+     {error in
+     if let error = error {
+     print("FAAAAAIIIIILLLLLL ")
+     }
+     else{
+     print("YESSSSSSSSSSS")
+     }
+     }}}*/
     func get(){
         let db = Firestore.firestore()
         db.collection("Unistudent").whereField("StudentEmail", isEqualTo: "441201198@student.ksu.edu.sa").getDocuments{
             (snapshot, error) in
             if let error = error {
-             print("FAIL ")
+                print("FAIL ")
             }
             else{
-            print("SUCCESS")
+                print("SUCCESS")
                 let actual = snapshot!.documents.first!.get("courses") as! [String]
                 let sects = snapshot!.documents.first!.get("Sections") as! [String]
                 print(actual)
                 for i in 0..<actual.count {
-                
+                    
                     let label = UIButton(frame: .init(x: self.view.frame.midX-120 , y: 200 + ( Double(i) * 70 ), width: 250, height: 50))
                     label.setTitle(actual[i], for: .normal)
                     label.setTitleColor(UIColor(red: 55/255, green: 84/255, blue: 151/255, alpha: 2), for: .normal)
                     label.backgroundColor = UIColor(red: 138/255, green: 176/255, blue: 183/255, alpha: 0.75)
-                  //label.params["course"] = actual[i]
+                    //label.params["course"] = actual[i]
                     //!!!!!!
                     label.tag = Int(sects[i]) ?? 0
                     label.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
@@ -52,27 +56,27 @@ class ViewController: UIViewController {
                     self.view.addSubview(label)
                 }
                 //Vstack
-               // coursesT.text = actual
-           //     print((actual).count)
+                // coursesT.text = actual
+                //     print((actual).count)
             }
         }
-
-}
+        
+    }
     
     @objc func pressed(sender:UIButton) {
-      
-        //1
-        let titleB = sender.title(for: .normal)
-        //2
-        let section = String(sender.tag)
         
-        self.performSegue(withIdentifier: "s1", sender: self)
-
+        //1
+        titleB = sender.title(for: .normal)!
+        //2
+        section = String(sender.tag)
+        
+        
+        
         let db = Firestore.firestore()
-         db.collection("Sections").whereField("section", isEqualTo: section).getDocuments{
+        db.collection("Sections").whereField("section", isEqualTo: section).getDocuments{
             (snapshot, error) in
             if let error = error {
-             print("FAIL2 ")
+                print("FAIL2 ")
             }
             else{
                 print("SUCCESS2")
@@ -82,21 +86,42 @@ class ViewController: UIViewController {
                 db.collection("Lecturer").whereField("id", isEqualTo: id).getDocuments{
                     (snapshot, error) in
                     if let error = error {
-                     print("FAIL3 ")
+                        print("FAIL3 ")
                     }
                     else{
-                    print("SUCCESS 3")
-                        let name = snapshot!.documents.first!.get("name") as! String
+                        print("SUCCESS 3")
+                        self.name = snapshot!.documents.first!.get("name") as! String
+                        self.performSegue(withIdentifier: "s1", sender: self)
                         //3
-                        print(name)
+                        //print(name)
                         
+//                        let c = UILabel(frame: .init(x: self.view.frame.midX-120 , y: 200 , width: 250, height: 50))
+//                        c.text = titleB
+//                        c.textColor = UIColor(red: 55/255, green: 84/255, blue: 151/255, alpha: 2)
+//
+//                        let s = UILabel(frame: .init(x: self.view.frame.midX-120 , y: 270 , width: 250, height: 50))
+//                        s.text = section
+//                        s.textColor = UIColor(red: 55/255, green: 84/255, blue: 151/255, alpha: 2)
+//
+//                        let n = UILabel(frame: .init(x: self.view.frame.midX-120 , y: 340 , width: 250, height: 50))
+//                        n.text = name
+//                        n.textColor = UIColor(red: 55/255, green: 84/255, blue: 151/255, alpha: 2)
                         
-                        let n = UILabel(frame: .init(x: self.view.frame.midX-120 , y: 340 , width: 250, height: 50))
-                        n.text = name
-                        n.textColor = UIColor(red: 55/255, green: 84/255, blue: 151/255, alpha: 2)
-        
+                    }
+                }
+            }
+        }
     }
-                }}}}
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "s1" {
+            if let controller = segue.destination as? DetailsViewController {
+                controller.section = section
+                controller.name = name
+                controller.titleB = titleB
+            }
+        }
+    }
     
     @IBAction func exit(segue: UIStoryboardSegue){
         print("exited")
@@ -104,7 +129,7 @@ class ViewController: UIViewController {
     
 }
 
-    
+
 
 /*
  let c = UILabel(frame: .init(x: self.view.frame.midX-120 , y: 200 , width: 250, height: 50))
