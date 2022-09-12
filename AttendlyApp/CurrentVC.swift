f56d7b37b7f16d1be8540a4a14736d9e1b0aed17
Yumna <67751566+yomna1998@users.noreply.github.com>
2022-09-11 21:12:39 -0700

    yumna Add button to the current page

--- AttendlyApp/CurrentVC.swift
+++ AttendlyApp/CurrentVC.swift
@@ -9,21 +9,41 @@ import UIKit
 
 class CurrentVC: UIViewController {
 
+    @IBOutlet weak var selectCourseBtn: UIButton!
+    
+    @IBOutlet var butsection1Collection: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        selectCourseBtn.layer.cornerRadius = selectCourseBtn.frame.height / 2
+        butsection1Collection.forEach{  (btn) in btn.layer.cornerRadius = btn.frame.height / 2
+            btn.layer.cornerRadius = btn.frame.height / 2
+            btn.isHidden = true
+            btn.alpha = 0
+        
+    }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func selectCourse(_ sender: UIButton) {
+        butsection1Collection.forEach{ (btn) in UIView.animate(withDuration: 0.7){
+            btn.isHidden = !btn.isHidden
+            btn.alpha = btn.alpha == 0 ? 1 : 0
+            btn.layoutIfNeeded()
+            
+        }
     }
-    */
-
+    }
+    @IBAction func butSection1(_ sender: UIButton) {
+        if let btnLb1 = sender.titleLabel?.text{
+            print(btnLb1)
+            
+        }
+        
+    }
+    
+   
 }
+
