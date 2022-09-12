eaea9ddc9f6cd0da2edc735d2988f9fcd765d6e0
sara81a <98774608+sara81a@users.noreply.github.com>
2022-09-13 01:25:40 +0300

    finally submit

--- AttendlyApp/loginController.swift
+++ AttendlyApp/loginController.swift
@@ -55,7 +55,7 @@ class loginController: UIViewController {
         }
         if password.count != 8 {
             validationMessegepass.isHidden = false
-            validationMessegepass.text = "Please enter password with 8 number"
+            validationMessegepass.text = "Please enter password with 8 number is"
             return (false, "", "")
         }
         
