668ce4dad201c67ab0d6371c90b8f40ee6669bc8
sara81a <98774608+sara81a@users.noreply.github.com>
2022-09-14 01:26:12 +0300

    try ???

---
+++ AttendlyApp/tryyy.swift
@@ -0,0 +1,29 @@
+//
+//  tryyy.swift
+//  AttendlyApp
+//
+//  Created by Sara Alsaleh on 18/02/1444 AH.
+//
+
+import UIKit
+
+class tryyy: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
